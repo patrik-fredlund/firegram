cf1e29b7d5981fb4e2d5333fcb44a0f6b57630d2
patrik-fredlund <patrikfredlunds@gmail.com>
2021-02-22 10:37:49 +0100

    added uploadform component

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import * as firebase from 'forebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBaus4WxN7DjEbA0FwwTBYshXvQSdM5Onc",
+    authDomain: "ninja-firegram-ced48.firebaseapp.com",
+    projectId: "ninja-firegram-ced48",
+    storageBucket: "ninja-firegram-ced48.appspot.com",
+    messagingSenderId: "359305346381",
+    appId: "1:359305346381:web:238152c79b501d1045cb04"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const projectStorage = firebase.storage();
+  const projectFirestore = firebase.firestore();
+
+  export { projectStorage, projectFirestore};
